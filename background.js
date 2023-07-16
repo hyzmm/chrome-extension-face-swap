12d900be538fb8aa6ee3623584ae4293a721b8f6
hyzm <990473625@qq.com>
2023-07-17 01:34:35 +0800

    chrome: Initialize the Chrome extension project

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener(async () => {
+    console.log(chrome.contextMenus.create)
+    chrome.contextMenus.create({
+        id: "face-swap",
+        title: "Face Swap", contexts: ["image"]
+    });
+});
+
+chrome.contextMenus.onClicked.addListener((item, tab) => {
+    debugger
+    const url = new URL(item.srcUrl);
+    chrome.tabs.create({ url: url.href, index: tab.index + 1 });
+});
