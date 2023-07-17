f3a59f0a2a9de11cbcacf9795a163caa158c1323
hyzm <990473625@qq.com>
2023-07-18 01:57:13 +0800

    wip

--- popup/src/App.jsx
+++ popup/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import exchange from "./assets/exchange.svg";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={"flex h-full flex-col"}>
+      <div className={"flex w-full grow  items-center"}>
+        <img
+          draggable={false}
+          src="https://img1.baidu.com/it/u=1719075340,4185340421&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500"
+          alt=""
+          className={"card h-full basis-1/2 object-contain"}
+        />
+        <img src={exchange} className={"white m-2 h-8 w-8"} />
+        <img
+          draggable={false}
+          src="https://img1.baidu.com/it/u=826797811,3494357996&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=535"
+          alt=""
+          className={"card  basis-1/2 object-contain"}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs underline flex">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <button className={"rounded-pill"}>Execute</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
